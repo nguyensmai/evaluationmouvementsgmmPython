810c1df4652af4a4ad3dd64ef4e9643108004685
libo0321 <38711049+libo0321@users.noreply.github.com>
2020-07-26 19:29:45 +0200

    Add files via upload

--- m_fcts/init_GMM_kbins.py
+++ m_fcts/init_GMM_kbins.py
@@ -8,10 +8,11 @@ def init_GMM_kbins(Data, model, nbSamples):
     """
     nbData = Data.shape[1] / nbSamples
     if model.params_diagRegFact == None:
-        model.params_diagRegFact = 1e-4 #Optional regularization term to avoid numerical instability
+        model.params_diagRegFact = 1e-4  # Optional regularization term to avoid numerical instability
 
     # Delimit the cluster bins for the first demonstration
-    tSep = np.round(np.linspace(0, nbData, model.nbStates + 1)).astype(int)  ##round function is not well defined in python
+    tSep = np.round(np.linspace(0, nbData, model.nbStates + 1)).astype(
+        int)  ##round function is not well defined in python
 
     # Compute statistics for each bin
     model.Priors = np.zeros(model.nbStates)
@@ -22,12 +23,12 @@ def init_GMM_kbins(Data, model, nbSamples):
     for i in range(0, model.nbStates):
         id = np.array([])
         for n in range(nbSamples):
-            id = np.hstack((id, n*nbData + np.array(range(tSep[i],tSep[i+1])))).astype(int)
+            id = np.hstack((id, n * nbData + np.array(range(tSep[i], tSep[i + 1])))).astype(int)
         model.Priors[i] = id.size
-        aee = np.mean(Data[:,id], axis=1,keepdims=True)
-        model.Mu[:,i:i+1] = np.mean(Data[:,id], axis=1, keepdims=True)
-        er = Data[:,id]
-        ae = np.cov(Data[:,id])+np.identity(Data.shape[0]) * model.params_diagRegFact
+        aee = np.mean(Data[:, id], axis=1, keepdims=True)
+        model.Mu[:, i:i + 1] = np.mean(Data[:, id], axis=1, keepdims=True)
+        er = Data[:, id]
+        ae = np.cov(Data[:, id]) + np.identity(Data.shape[0]) * model.params_diagRegFact
 
-        model.Sigma[:,:,i] = np.cov(Data[:,id]) + np.identity(Data.shape[0]) * model.params_diagRegFact
+        model.Sigma[:, :, i] = np.cov(Data[:, id]) + np.identity(Data.shape[0]) * model.params_diagRegFact
     model.Priors = model.Priors / np.sum(model.Priors)
