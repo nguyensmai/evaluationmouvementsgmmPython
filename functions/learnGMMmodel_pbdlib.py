91c1b26f1d68bd28f9db5f449c8d358b6a4868f6
libo0321 <38711049+libo0321@users.noreply.github.com>
2020-08-16 23:28:07 +0200

    Add files via upload

--- functions/learnGMMmodel_pbdlib.py
+++ functions/learnGMMmodel_pbdlib.py
@@ -1,107 +1,35 @@
 import numpy as np
-from m_fcts.init_GMM_kbins import init_GMM_kbins
-from m_fcts.gaussPDF import gaussPDF
-from functions.mapping import logmap, expmap
+import riepybdlib.manifold as rm   # For the manifold functionality
+import riepybdlib.statistics as rs # For statistics on riemannian manifolds
+from riepybdlib.angular_representations import Quaternion
+import time
 
 
-def learnGMMmodel_pbdlib(model, u, xIn, xOut, nbSamples, nbIterEM, nbIter, nbData):
+def learnGMMmodel_pbdlib(model, xIn, xOut, nbStates, nbIterEM):
+    m_3deucl = rm.get_euclidean_manifold(3, '3d-eucl')
+    m_time = rm.get_euclidean_manifold(1, 'time')
+    m_quat = rm.get_quaternion_manifold('orientation')
 
-    init_GMM_kbins(u, model, nbSamples)
-    # MuMan
-    mu_exp = np.array([[0], [1], [0], [0]])
-    model.MuMan = np.zeros((55, model.nbStates))  ### if we change the number of Variables, 55 needs to be modified
-    model.MuMan[0, :] = model.Mu[0, :]
-    model.MuMan[1:5, :] = expmap(model.Mu[1:4], mu_exp)
-    model.MuMan[5:9, :] = expmap(model.Mu[4:7], mu_exp)
-    model.MuMan[9:13, :] = expmap(model.Mu[7:10], mu_exp)
-    model.MuMan[13:17, :] = expmap(model.Mu[10:13], mu_exp)
-    model.MuMan[17:21, :] = expmap(model.Mu[13:16], mu_exp)
-    model.MuMan[21:25, :] = expmap(model.Mu[16:19], mu_exp)
-    model.MuMan[25:29, :] = expmap(model.Mu[19:22], mu_exp)
-    model.MuMan[29:33, :] = expmap(model.Mu[22:25], mu_exp)
-    model.MuMan[33:37, :] = expmap(model.Mu[25:28], mu_exp)
-    model.MuMan[37:55, :] = model.Mu[28:46, :]
-    # Mu
-    model.Mu = np.zeros((model.nbVar, model.nbStates)) ## ???
+    xIn = np.array([xIn]).T
+    x = (xIn, Quaternion.from_nparray(xOut[1].T), Quaternion.from_nparray(xOut[2].T), Quaternion.from_nparray(xOut[3].T)
+         , Quaternion.from_nparray(xOut[4].T), Quaternion.from_nparray(xOut[5].T), Quaternion.from_nparray(xOut[6].T)
+         , Quaternion.from_nparray(xOut[7].T), Quaternion.from_nparray(xOut[8].T), Quaternion.from_nparray(xOut[9].T),
+         xOut[10].T, xOut[11].T, xOut[12].T, xOut[13].T, xOut[14].T, xOut[15].T)
 
-    uTmp = np.zeros((model.nbVar, nbData * nbSamples, model.nbStates))
-    avg_loglik = np.zeros(nbIterEM)
-    Data_gaussPDF = np.zeros((model.nbVar, xIn.size))  ## or xIn.shape[1]
-    U0 = np.zeros((model.nbVar, model.nbVar, model.nbStates),dtype=complex)  ## U0 is complex
-    for nb in range(nbIterEM):
-        L = np.zeros((model.nbStates, model.x.shape[1]))
-        for i in range(model.nbStates):
-            Data_gaussPDF[0, :] = xIn - model.MuMan[0, i]
-            Data_gaussPDF[1:4, :] = logmap(xOut[1], model.MuMan[1:5, i])
-            Data_gaussPDF[4:7, :] = logmap(xOut[2], model.MuMan[5:9, i])
-            Data_gaussPDF[7:10, :] = logmap(xOut[3], model.MuMan[9:13, i])
-            Data_gaussPDF[10:13, :] = logmap(xOut[4], model.MuMan[13:17, i])
-            Data_gaussPDF[13:16, :] = logmap(xOut[5], model.MuMan[17:21, i])
-            Data_gaussPDF[16:19, :] = logmap(xOut[6], model.MuMan[21:25, i])
-            Data_gaussPDF[19:22, :] = logmap(xOut[7], model.MuMan[25:29, i])
-            Data_gaussPDF[22:25, :] = logmap(xOut[8], model.MuMan[29:33, i])
-            Data_gaussPDF[25:28, :] = logmap(xOut[9], model.MuMan[33:37, i])
-            Data_gaussPDF[28:31, :] = xOut[10] - model.MuMan[37:40, i:i + 1]
-            Data_gaussPDF[31:34, :] = xOut[11] - model.MuMan[40:43, i:i + 1]
-            Data_gaussPDF[34:37, :] = xOut[12] - model.MuMan[43:46, i:i + 1]
-            Data_gaussPDF[37:40, :] = xOut[13] - model.MuMan[46:49, i:i + 1]
-            Data_gaussPDF[40:43, :] = xOut[14] - model.MuMan[49:52, i:i + 1]
-            Data_gaussPDF[43:46, :] = xOut[15] - model.MuMan[52:55, i:i + 1]
+    GMM = rs.GMM(m_time * m_quat * m_quat * m_quat * m_quat * m_quat * m_quat * m_quat * m_quat * m_quat
+                 * m_3deucl * m_3deucl * m_3deucl * m_3deucl * m_3deucl * m_3deucl, n_components=nbStates)
 
-            L[i, :] = model.Priors[i] * gaussPDF(Data_gaussPDF, model.Mu[:, i], model.Sigma[:, :, i])
+    GMM.init_time_based(x[0], x, reg_lambda=model.params_diagRegFact, reg_type=rs.RegularizationType.DIAGONAL)
 
-        GAMMA = L / (np.sum(L, axis=0, keepdims=True) + np.finfo(float).tiny)
-        GAMMA2 = GAMMA / np.sum(GAMMA, axis=1, keepdims=True)
-        GAMMA2 = np.where(np.isnan(GAMMA2), 0, GAMMA2)
-        avg_loglik[nb] = -np.log(np.mean(np.sum(L, axis=0)))
-        # M-step
-        for i in range(model.nbStates):
-            # Update Priors
-            model.Priors[i] = np.sum(GAMMA[i, :]) / (nbData * nbSamples)
-            # Update MuMan
-            for n in range(nbIter):
-                uTmp[0, :, i] = xIn - model.MuMan[0, i]
-                uTmp[1:4, :, i] = logmap(xOut[1], model.MuMan[1:5, i])
-                uTmp[4:7, :, i] = logmap(xOut[2], model.MuMan[5:9, i])
-                uTmp[7:10, :, i] = logmap(xOut[3], model.MuMan[9:13, i])
-                uTmp[10:13, :, i] = logmap(xOut[4], model.MuMan[13:17, i])
-                uTmp[13:16, :, i] = logmap(xOut[5], model.MuMan[17:21, i])
-                uTmp[16:19, :, i] = logmap(xOut[6], model.MuMan[21:25, i])
-                uTmp[19:22, :, i] = logmap(xOut[7], model.MuMan[25:29, i])
-                uTmp[22:25, :, i] = logmap(xOut[8], model.MuMan[29:33, i])
-                uTmp[25:28, :, i] = logmap(xOut[9], model.MuMan[33:37, i])
-                uTmp[28:31, :, i] = xOut[10] - model.MuMan[37:40, i:i + 1]
-                uTmp[31:34, :, i] = xOut[11] - model.MuMan[40:43, i:i + 1]
-                uTmp[34:37, :, i] = xOut[12] - model.MuMan[43:46, i:i + 1]
-                uTmp[37:40, :, i] = xOut[13] - model.MuMan[46:49, i:i + 1]
-                uTmp[40:43, :, i] = xOut[14] - model.MuMan[49:52, i:i + 1]
-                uTmp[43:46, :, i] = xOut[15] - model.MuMan[52:55, i:i + 1]
+    t1 = time.perf_counter()
+    GMM.fit(x, maxsteps=nbIterEM, reg_lambda=model.params_diagRegFact, reg_type=rs.RegularizationType.DIAGONAL)
+    t2 = time.perf_counter()
+    print("function fit() takes ", t2 - t1, 's')
 
-                GAMMA2_col = GAMMA2[i:i + 1, :].T
-                model.MuMan[0, i] = np.dot(model.MuMan[0, i] + uTmp[0, :, i], GAMMA2_col)
-                model.MuMan[1:5, i:i + 1] = expmap(np.dot(uTmp[1:4, :, i], GAMMA2_col), model.MuMan[1:5, i])
-                model.MuMan[5:9, i:i + 1] = expmap(np.dot(uTmp[4:7, :, i], GAMMA2_col), model.MuMan[5:9, i])
-                model.MuMan[9:13, i:i + 1] = expmap(np.dot(uTmp[7:10, :, i], GAMMA2_col), model.MuMan[9:13, i])
-                model.MuMan[13:17, i:i + 1] = expmap(np.dot(uTmp[10:13, :, i], GAMMA2_col), model.MuMan[13:17, i])
-                model.MuMan[17:21, i:i + 1] = expmap(np.dot(uTmp[13:16, :, i], GAMMA2_col), model.MuMan[17:21, i])
-                model.MuMan[21:25, i:i + 1] = expmap(np.dot(uTmp[16:19, :, i], GAMMA2_col), model.MuMan[21:25, i])
-                model.MuMan[25:29, i:i + 1] = expmap(np.dot(uTmp[19:22, :, i], GAMMA2_col), model.MuMan[25:29, i])
-                model.MuMan[29:33, i:i + 1] = expmap(np.dot(uTmp[22:25, :, i], GAMMA2_col), model.MuMan[29:33, i])
-                model.MuMan[33:37, i:i + 1] = expmap(np.dot(uTmp[25:28, :, i], GAMMA2_col), model.MuMan[33:37, i])
-                model.MuMan[37:40, i:i + 1] = np.dot(model.MuMan[37:40, i:i + 1] + uTmp[28:31, :, i], GAMMA2_col)
-                model.MuMan[40:43, i:i + 1] = np.dot(model.MuMan[40:43, i:i + 1] + uTmp[31:34, :, i], GAMMA2_col)
-                model.MuMan[43:46, i:i + 1] = np.dot(model.MuMan[43:46, i:i + 1] + uTmp[34:37, :, i], GAMMA2_col)
-                model.MuMan[46:49, i:i + 1] = np.dot(model.MuMan[46:49, i:i + 1] + uTmp[37:40, :, i], GAMMA2_col)
-                model.MuMan[49:52, i:i + 1] = np.dot(model.MuMan[49:52, i:i + 1] + uTmp[40:43, :, i], GAMMA2_col)
-                model.MuMan[52:55, i:i + 1] = np.dot(model.MuMan[52:55, i:i + 1] + uTmp[43:46, :, i], GAMMA2_col)
-            # Update Sigma
-            model.Sigma[:, :, i] = uTmp[:, :, i].dot(np.diag(GAMMA2[i, :])).dot(uTmp[:, :, i].T) + np.identity(
-                u.shape[0]) * model.params_diagRegFact
-
-    # Eigendecomposition of Sigma
-    for i in range(model.nbStates):
-        W, V = np.linalg.eig(model.Sigma[:, :, i]) ##np.linalg.eig doesn't return exactly the same result as eig() in matlab
-        D_sqrt = np.diag(np.sqrt(W))
-        U0[:, :, i] = np.dot(V, D_sqrt)
-    model.U0 = U0
-    pass
+    model.Mu = np.zeros((model.nbVar, model.nbStates))
+    model.Priors = GMM.priors
+    model.Sigma = np.zeros((model.nbVar, model.nbVar, nbStates))
+    model.MuMan = np.zeros((model.nbVarMan, nbStates))
+    for i, gauss in enumerate(GMM.gaussians):
+        model.Sigma[:, :, i] = gauss.sigma
+        model.MuMan[:, i] = gauss.manifold.manifold_to_np(gauss.mu)
--- functions/processTrainingData_pbdlib.py
+++ functions/processTrainingData_pbdlib.py
@@ -4,15 +4,12 @@ from functions.temporalAlignment import temporalAlignment
 from riepybdlib.manifold import get_euclidean_manifold, get_quaternion_manifold
 
 
-def processTrainingData_pbdlib(model, trainName, nspp, registration, fastDP, filt, est, rem, ws, nbData):
+def processTrainingData_pbdlib(model, trainName, nbSamples, registration, fastDP, filt, est, rem, ws, nbData):
     xIn = np.array([])
-    uOut = {}
     uRef = {}
     xOut = {}
-    for i in range(1, 16):  # this dictionary starts with 1 and ends with 15
-        uOut[i] = np.array([])
 
-    for i in range(nspp):
+    for i in range(nbSamples):
         fname = 'SkeletonSequence' + str(i + 1) + '.txt'
         dataTrain = loadData(trainName, fname, filt, est, rem, ws, nbData)[2]  ##0.7 second
         out = dataTrain['lElbow ori']
@@ -39,31 +36,4 @@ def processTrainingData_pbdlib(model, trainName, nspp, registration, fastDP, fil
             else:
                 xOut[k] = np.hstack((xOut[k], dataTrain[d]))
             k += 1
-    uIn = xIn  ## x, u structure optimised
-    std = np.array([[0], [1], [0], [0]])
-    for k in range(1, 16):
-        if k < 10:
-            uOut[k] = logmap(xOut[k], std)
-        else:
-            uOut[k] = xOut[k]
-
-    m_time = get_euclidean_manifold('time')  # actually it's 'in' in matlab
-    m1 = get_quaternion_manifold('lElbow ori')
-    m2 = get_quaternion_manifold('lWrist ori')
-    m3 = get_quaternion_manifold('lShoulder ori')
-    m4 = get_quaternion_manifold('rElbow ori')
-    m5 = get_quaternion_manifold('rWrist ori')
-    m6 = get_quaternion_manifold('rShoulder ori')
-    m7 = get_quaternion_manifold('mSpine ori')
-    m8 = get_quaternion_manifold('mShoulder ori')
-    m9 = get_quaternion_manifold('Neck ori')
-    m10 = get_euclidean_manifold(3, 'lElbow rel_pos')
-    m11 = get_euclidean_manifold(3, 'lWrist rel_pos')
-    m12 = get_euclidean_manifold(3, 'lShoulder rel_pos')
-    m13 = get_euclidean_manifold(3, 'rElbow rel_pos')
-    m14 = get_euclidean_manifold(3, 'rWrist rel_pos')
-    m15 = get_euclidean_manifold(3, 'rShoulder rel_pos')
-
-    manifold = m_time * m1 * m2 * m3 * m4 * m5 * m6 * m7 * m8 * m9 * m10 * m11 * m12 * m13 * m14 * m15
-
-    return xIn, uIn, xOut, uOut
+    return xIn, xOut
